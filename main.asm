2febe9da7b93eba22892d4c5e249d0a1d14f11ba
nghugo <nghugoryan@gmail.com>
2023-11-17 10:38:11 +0000

    add isPrime

--- main.asm
+++ main.asm
@@ -15,11 +15,10 @@ main:
     li $v0, 4       # syscall: print string
     la $a0, greeting_message  # load address of prompt string
     syscall
-
-
 main_loop:
     jal get_option
     j main_loop
+
 
 
 get_option:
@@ -67,13 +66,11 @@ option_executed_or_invalid_input:
 
 
 is_prime:
-    # if call procedure, make sure to store $ra in stack
-    # TODO: is_prime procedure
-    # TODO: get_remainder procedure
 
-    addi $sp, $sp, -8  # Move the stack pointer.
-    sw  $ra,    4($sp)  # Store return address (we are about to call nested procedures).
-    sw  $s0,    0($sp)  # Store $s0 as we want to use it, but we are also obligated to restore it.
+    addi $sp, $sp, -12  # Move the stack pointer.
+    sw  $ra,    8($sp)  # Store return address (we are about to call nested procedures).
+    sw  $s0,    4($sp)  # Store $s0 as we want to use it, but we are also obligated to restore it.
+    sw  $s1,    0($sp)  # Store $s1 as we want to use it, but we are also obligated to restore it.
 
     # Display is_prime_message (isPrime?)
     li $v0, 4       # syscall: print string
@@ -83,32 +80,27 @@ is_prime:
     # Receive integer from user input
     li $v0, 5       # syscall: read integer
     syscall
+    add $s0, $v0, $zero  # store user's input integer (dividend) in $s0, which persists across procedural calls
 
-    # Set received integer to $s0, which persists across procedural calls
-    add $s0, $v0, $zero
+    li $t0, 1  # just set a threshold for comparison
+    ble $s0, $t0, is_prime_false  # if $s0 ie integer <= 1, jump to is_prime_false   
+    li $s1, 2  # divisor starts at 2 and is gradually increased to the value of the dividend
 
+is_prime_loop:
+    beq $s0, $s1, is_prime_true  # if the divisor $s1 reaches the dividend $s0 without ever getting remainder 0, then exit to is_prime_true
+    add $a0, $s0, $zero  # dividend
+    add $a1, $s1, $zero  # divisor
+    jal get_remainder  # get_remainder takes $a0 as the dividend, $a1 as the divisor, and returns remainder in $v0
+    beq $v0, $zero, is_prime_false  # exit to is_prime_false if remainder is 0 (indicates dividend is not prime)
+    addi $s1, $s1, 1  # increment divisor by 1
+    j is_prime_loop
+
+is_prime_true:   
     # Display user's input integer of interest (stored in $s0)
     li $v0, 1       # syscall: print integer
     add $a0, $s0, $zero
     syscall
 
-    li $t0, 1
-    ble $s0, $t0, is_prime_false  # if $s0 ie integer <= 1, jump to is_prime_false   
-    # TODO: unfin, currently all values <= 1 are not prime and all values > 1 are prime
-
-    # TODO: call get_remainder function  # # # # # #  # # # # # #  # # # # # #  # # # # # #
-    add $a0, $s0, $zero  # value
-    # add $a1, $s0, $zero  # divisor  # TODO
-    li $a1, 4  # TODO remove
-    jal get_remainder
-    
-    # TODO remove
-    add $a0, $v0, $zero  # value of get_remainder
-    li $v0, 1       # syscall: print integer
-    syscall
-    # # # # # #  # # # # # #  # # # # # #  # # # # # #  # # # # # #  # # # # # #  # # # # # #
-
-is_prime_true:   
     # Display is_prime_true_message
     li $v0, 4       # syscall: print string
     la $a0, is_prime_true_message  # load address of prompt string
@@ -116,26 +108,33 @@ is_prime_true:
     j is_prime_done
 
 is_prime_false:
+    # Display user's input integer of interest (stored in $s0)
+    li $v0, 1       # syscall: print integer
+    add $a0, $s0, $zero
+    syscall
+
     # Display is_prime_false_message
     li $v0, 4       # syscall: print string
     la $a0, is_prime_false_message  # load address of prompt string
     syscall
 
 is_prime_done:
-    lw      $ra,        4($sp)      # Restore return address.
-    lw      $s0,        0($sp)      # Restore $s0.
-    addi    $sp,        $sp,    8   # Restore stack pointer position.
+    lw      $ra,        8($sp)      # Restore return address.
+    lw      $s0,        4($sp)      # Restore $s0.
+    lw      $s1,        0($sp)      # Restore $s1.
+    addi    $sp,        $sp,    12   # Restore stack pointer position.
     jr $ra
 
 
+
 get_remainder:
-    # take args from $a0 (val) and $a1 (divisor)
+    # take args from $a0 (dividend) and $a1 (divisor)
     # store remainder in $v0
     li $t0, 0
-    blt $a0, $t0, get_remainder_exit  # if $a0 (val) < 0, jump to get_remainder
+    blt $a0, $t0, get_remainder_done  # if $a0 (dividend) < 0, jump to get_remainder
     sub $a0, $a0, $a1
     j get_remainder  # loop until exit
-get_remainder_exit:
+get_remainder_done:
     add $v0, $a0, $a1  # correct for overshoot, store result in $v0
     jr $ra
 
