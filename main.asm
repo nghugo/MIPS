be0641b9ad10124e21628fcd289bca5039d6f24b
nghugo <nghugoryan@gmail.com>
2023-11-17 12:23:19 +0000

    edit code and display spacing

--- main.asm
+++ main.asm
@@ -1,10 +1,10 @@
 .data
 greeting_message:    .asciiz "Welcome to the MIPS Calculator\n"
 select_option_message:    .asciiz "\nSelect an option:\n[1] isPrime?    [2] Factorial    [3] Exit\n"
-is_prime_message:    .asciiz "-------\nisPrime\n-------\nPlease enter an integer:\n"
-is_prime_true_message:    .asciiz " is a prime number\n"
-is_prime_false_message:    .asciiz " is NOT a prime number\n"
-factorial_message:    .asciiz "-------\nFactorial\n-------\nPlease enter an integer:\n"
+is_prime_message:    .asciiz "-------\nisPrime?\n-------\nPlease enter an integer: "
+is_prime_true_message:    .asciiz " is a prime number.\n"
+is_prime_false_message:    .asciiz " is NOT a prime number.\n"
+factorial_message:    .asciiz "-------\nFactorial\n-------\nPlease enter an integer: "
 factorial_of_message:    .asciiz "Factorial of "
 factorial_is_message:    .asciiz " is "
 newline_message:     .asciiz "\n"
@@ -15,7 +15,7 @@ exit_message:    .asciiz "Exit.\n"
 
 main:
     # Greet user
-    li $v0, 4       # syscall: print string
+    li $v0, 4  # syscall: print string
     la $a0, greeting_message  # load address of prompt string
     syscall
 main_loop:
@@ -27,16 +27,16 @@ main_loop:
 get_option:
 
     addi $sp, $sp, -8  # Move the stack pointer.
-    sw  $ra,    4($sp)  # Store return address (we are about to call nested procedures).
-    sw  $s0,    0($sp)  # Store $s0 as we want to use it, but we are also obligated to restore it.
+    sw  $ra, 4($sp)  # Store return address (we are about to call nested procedures).
+    sw  $s0, 0($sp)  # Store $s0 as we want to use it, but we are also obligated to restore it.
 
     # Display select_option_message
-    li $v0, 4       # syscall: print string
+    li $v0, 4  # syscall: print string
     la $a0, select_option_message  # load address of prompt string
     syscall
 
     # Receive option from user input (expect integer 1/ 2/ 3)
-    li $v0, 5       # syscall: read integer
+    li $v0, 5  # syscall: read integer
     syscall
 
     # Set the option to $s0, which persists across procedural calls
@@ -62,9 +62,9 @@ option_2_or_3:
 option_3:
     j exit  # Value of option from user input is found to be 3, so execute exit
 option_executed_or_invalid_input:
-    lw      $ra,        4($sp)      # Restore return address.
-    lw      $s0,        0($sp)      # Restore $s0.
-    addi    $sp,        $sp,    8   # Restore stack pointer position.
+    lw $ra, 4($sp)  # Restore return address.
+    lw $s0, 0($sp)  # Restore $s0.
+    addi $sp, $sp, 8   # Restore stack pointer position.
     jr $ra # Return from program    
 
 
@@ -73,17 +73,17 @@ is_prime:
     # $s1 -> divisor (starts from 2 up to the dividend value)
 
     addi $sp, $sp, -12  # Move the stack pointer.
-    sw  $ra,    8($sp)  # Store return address (we are about to call nested procedures).
-    sw  $s0,    4($sp)  # Store $s0 as we want to use it, but we are also obligated to restore it.
-    sw  $s1,    0($sp)  # Store $s1 as we want to use it, but we are also obligated to restore it.
+    sw $ra, 8($sp)  # Store return address (we are about to call nested procedures).
+    sw $s0, 4($sp)  # Store $s0 as we want to use it, but we are also obligated to restore it.
+    sw $s1, 0($sp)  # Store $s1 as we want to use it, but we are also obligated to restore it.
 
     # Display is_prime_message (isPrime?)
-    li $v0, 4       # syscall: print string
+    li $v0, 4  # syscall: print string
     la $a0, is_prime_message  # load address of prompt string
     syscall
 
     # Receive integer from user input
-    li $v0, 5       # syscall: read integer
+    li $v0, 5  # syscall: read integer
     syscall
     add $s0, $v0, $zero  # store user's input integer (dividend) in $s0, which persists across procedural calls
 
@@ -102,32 +102,32 @@ is_prime_loop:
 
 is_prime_true:   
     # Display user's input integer of interest (stored in $s0)
-    li $v0, 1       # syscall: print integer
+    li $v0, 1  # syscall: print integer
     add $a0, $s0, $zero
     syscall
 
     # Display is_prime_true_message
-    li $v0, 4       # syscall: print string
+    li $v0, 4  # syscall: print string
     la $a0, is_prime_true_message  # load address of prompt string
     syscall
     j is_prime_done
 
 is_prime_false:
     # Display user's input integer of interest (stored in $s0)
-    li $v0, 1       # syscall: print integer
+    li $v0, 1  # syscall: print integer
     add $a0, $s0, $zero
     syscall
 
     # Display is_prime_false_message
-    li $v0, 4       # syscall: print string
+    li $v0, 4  # syscall: print string
     la $a0, is_prime_false_message  # load address of prompt string
     syscall
 
 is_prime_done:
-    lw      $ra,        8($sp)      # Restore return address.
-    lw      $s0,        4($sp)      # Restore $s0.
-    lw      $s1,        0($sp)      # Restore $s1.
-    addi    $sp,        $sp,    12   # Restore stack pointer position.
+    lw $ra, 8($sp)  # Restore return address.
+    lw $s0, 4($sp)  # Restore $s0.
+    lw $s1, 0($sp)  # Restore $s1.
+    addi $sp, $sp, 12   # Restore stack pointer position.
     jr $ra
 
 
@@ -151,25 +151,25 @@ factorial:
     # $s2 -> res
 
     addi $sp, $sp, -16  # Move the stack pointer.
-    sw  $ra,    12($sp)  # Store return address (we are about to call nested procedures).
-    sw  $s0,    8($sp)  # Store $s0 as we want to use it, but we are also obligated to restore it.
-    sw  $s1,    4($sp)  # Store $s1 as we want to use it, but we are also obligated to restore it.
-    sw  $s2,    0($sp)  # Store $s2 as we want to use it, but we are also obligated to restore it.
+    sw $ra, 12($sp)  # Store return address (we are about to call nested procedures).
+    sw $s0, 8($sp)  # Store $s0 as we want to use it, but we are also obligated to restore it.
+    sw $s1, 4($sp)  # Store $s1 as we want to use it, but we are also obligated to restore it.
+    sw $s2, 0($sp)  # Store $s2 as we want to use it, but we are also obligated to restore it.
 
     # Display factorial_message (Factorial)
-    li $v0, 4       # syscall: print string
+    li $v0, 4   # syscall: print string
     la $a0, factorial_message  # load address of prompt string
     syscall
 
     # Receive integer from user input
-    li $v0, 5       # syscall: read integer
+    li $v0, 5   # syscall: read integer
     syscall
     add $s0, $v0, $zero  # store user's input integer (factorial number) in $s0, which persists across procedural calls
     add $s1, $v0, $zero  # store user's input integer (factorial number) in $s1, which persists across procedural calls
-    li $s2, 1       # store value of 1 in $s2
+    li $s2, 1   # store value of 1 in $s2
 
 factorial_loop:
-    li $t0, 1       # load a threshold
+    li $t0, 1  # load a threshold
     ble $s0, $t0, factorial_done  # exit loop when value reaches 1
 
     # mult x * y performs better if we pick $a1 to be the smaller number out of x and y
@@ -189,35 +189,35 @@ factorial_done:
     # $s2 -> res
     
     # Display factorial_of_message (Factorial of)
-    li $v0, 4       # syscall: print string
+    li $v0, 4   # syscall: print string
     la $a0, factorial_of_message  # load address of prompt string
     syscall
 
     # Display the original factorial number (stored in $s1)
-    li $v0, 1       # syscall: print integer
+    li $v0, 1  # syscall: print integer
     add $a0, $s1, $zero
     syscall
     
     # Display factorial_is_message (is)
-    li $v0, 4       # syscall: print string
+    li $v0, 4  # syscall: print string
     la $a0, factorial_is_message  # load address of prompt string
     syscall
 
     # Display the result (stored in $s2)
-    li $v0, 1       # syscall: print integer
+    li $v0, 1  # syscall: print integer
     add $a0, $s2, $zero
     syscall
 
-    # Display newline_message (\n)
-    li $v0, 4       # syscall: print string
+    # Display newline_message (.\n)
+    li $v0, 4  # syscall: print string
     la $a0, newline_message  # load address of prompt string
     syscall
 
-    lw      $ra,        12($sp)      # Restore return address.
-    lw      $s0,        8($sp)      # Restore $s0.
-    lw      $s1,        4($sp)      # Restore $s1.
-    lw      $s2,        4($sp)      # Restore $s2.
-    addi    $sp,        $sp,    16   # Restore stack pointer position.
+    lw $ra, 12($sp)  # Restore return address.
+    lw $s0, 8($sp)  # Restore $s0.
+    lw $s1, 4($sp)  # Restore $s1.
+    lw $s2, 4($sp)  # Restore $s2.
+    addi $sp, $sp, 16  # Restore stack pointer position.
     jr $ra
 
 
@@ -240,8 +240,8 @@ mult_done:
 
 exit:
     # Display select_option_message
-    li $v0, 4       # syscall: print string
+    li $v0, 4  # syscall: print string
     la $a0, exit_message  # load address of prompt string
     syscall
-    li $v0, 10      # syscall: exit
+    li $v0, 10  # syscall: exit
     syscall
